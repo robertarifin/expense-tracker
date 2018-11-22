99e09c000563de09a782e9f76a6e79be452817aa
Desy Armariena <desy.armariena@gmail.com>
2018-11-22 22:34:40 +0700

    Validation password length

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -191,14 +191,18 @@ class TransactionController {
     }
 
     static deleteExpense(req, res) {
-        Model.Transaction.destroy({where: {id: req.params.transactionId}})
+        Model.Transaction.destroy(
+            {
+                where: {id: req.params.transactionId},
+                individualHooks: true
+            }
+        )
         .then(() => {
             res.redirect('/transaction');
         })
         .catch(err => {
             res.send(err);
-        })
-        // res.send(req.params);
+        });
     }
 }
 
